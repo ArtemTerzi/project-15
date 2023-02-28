de5f96972ff3a7075d8e6fd2e0bf470582448006
Artem Terzi <terzi.artem.yuriyovych@gmail.com>
2023-02-28 23:32:49 +0200

    Parcials added

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './js/refs';
+import './js/categoryListMaker';
+import './js/createMarkup';
+import './js/favorite';
+import './js/fetchData';
+import './js/filter';
+import './js/themeSwaper';
---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+const refs = {};
+
+const options = {
+  API_KEY: 'MCCbLUuNkLgrOf1uBr1c9zmSoKm3Mp9g',
+};
+
+export default { refs, options };
