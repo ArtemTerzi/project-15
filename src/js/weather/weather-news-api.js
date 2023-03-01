aa80c992733cdaf58b1213987e81ea354df84ae0
Bogdan Ryzhko <m3meizumini@gmail.com>
2023-03-01 15:21:42 +0200

    add spin animaton

--- src/js/weather/weather-news-api.js
+++ src/js/weather/weather-news-api.js
@@ -1,16 +1,26 @@
 import { setLocalItem } from "./local-storage-api";
 import { getWeatherMurkup } from "./waether-markup";
+import { addSpinner, removeSpinner } from "./spinner";
 
 const weatherElem = document.querySelector(".weather");
 
 const API_KEY = "c76da9d16a38d15146607ff522b06c37";
 const USER_LOCATION_ITEM = "User-location";
 
+function checkWeather() {
+	if (weatherElem.children.length === 0) {
+		addSpinner(weatherElem);
+		weatherElem.classList.add("load");
+	} else {
+		removeSpinner(weatherElem);
+	}
+}
+
 navigator.geolocation.getCurrentPosition(async position => {
 	try {
+		const preloadWrapper = document.querySelector(".preload-wrapper");
 		const latitude = await position.coords.latitude;
 		const longitude = await position.coords.longitude;
-
 		const locationObj = { latitude, longitude };
 		setLocalItem(USER_LOCATION_ITEM, JSON.stringify(locationObj));
 
@@ -19,8 +29,16 @@ navigator.geolocation.getCurrentPosition(async position => {
 		const resopnse = await fetch(BASE_URL);
 		const data = await resopnse.json();
 		await console.log(data);
-		await weatherElem.insertAdjacentHTML("beforeend", getWeatherMurkup(data))
+		await weatherElem.insertAdjacentHTML("beforeend", getWeatherMurkup(data));
+
+		if (weatherElem.children.length > 0) {
+			weatherElem.classList.remove("load");
+			preloadWrapper.classList.remove("load");
+			removeSpinner(preloadWrapper);
+		}
 	} catch (error) {
 		console.log(error);
 	}
 });
+
+checkWeather();
