7a193d96402f1939bd7f46f08f7c3dde9cd5c4ba
AnnRefr <Ann_malaza@ukr.net>
2023-03-03 17:25:00 +0200

    in process

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import './js/fetchData';
 import './js/filter';
 import './js/themeSwaper';
 import './js/paginator';
+import './js/modal';
 // import './js/weather/weather';
 import { startWeather } from './js/weather/weather';
 import { formToJSON } from 'axios';
