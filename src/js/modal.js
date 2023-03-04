4614f3fa98663bd04ec092137232563fb0051a9d
AnnRefr <Ann_malaza@ukr.net>
2023-03-04 23:40:24 +0200

    fix-styles/add-js

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,15 +1,17 @@
-// (() => {
+(() => {
     
-//     const openModalBtn = document.querySelector('[data-menu-btn]');
-//     const mobMenu = document.querySelector('[data-menu]');
+    const modalBtn = document.querySelector('[data-menu-btn]');
+    const mobMenu = document.querySelector('[data-menu]');
 
-//     openModalBtn.addEventListener('click', toggleModal);
-//     mobMenu.addEventListener('click', toggleModal);
+    modalBtn.addEventListener('click', toggleModal);
+    mobMenu.addEventListener('click', toggleModal);
   
-//     function toggleModal() {
-//       mobMenu.classList.toggle('is-closed');
-//     }
-//   })();
+    function toggleModal() {
+      modalBtn.classList.toggle('active');  
+      mobMenu.classList.toggle('is-closed');
+      
+    }
+  })();
 
 
 
