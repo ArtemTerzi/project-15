b1d2b90da77714460ae944a6e20d2d0bb3d273d1
Oleksii <al.dimitrash@gmail.com>
2023-03-04 13:28:07 +0200

    update error page

---
+++ src/js/error.js
@@ -0,0 +1,33 @@
+function getMarkupError() {
+  return `<div class="error">
+  <h2 class="error-text">We haven’t found news from this category</h2>
+  <picture class="error-image">
+    <source
+      srcset="
+        ./images-error/error_screen_min.png 1x,
+        ./images-error/error_screen_x2.png  2x
+      "
+      media="(min-width: 1200px)"
+    />
+    <source
+      srcset="
+        ./images-error/error_tablet_min.png 1x,
+        ./images-error/error_tablet_x2.png  2x
+      "
+      media="(min-width: 768px)"
+    />
+    <source
+      srcset="
+        ./images-error/error_mobile_min.png 1x,
+        ./images-error/error_mobile_x2.png  2x
+      "
+      media="(max-width: 767px)"
+    />
+    <img
+      src="./images-error/error_screen_min.png"
+      alt="Error page"
+      width="601"
+    />
+  </picture>
+</div>`;
+}
