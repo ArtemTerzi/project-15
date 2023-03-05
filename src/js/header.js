1ec3e9ca58925e5ad05fa22893eef3b24cec990a
AntonAnatoliyovich <anton.shadura@gmail.com>
2023-03-05 14:37:22 +0200

    finish

--- src/js/header.js
+++ src/js/header.js
@@ -9,3 +9,14 @@ search.addEventListener('click', function(e) {
 header.addEventListener('click', function() {
     search.classList.remove('search--active')
 });
+
+
+
+const menu = document.getElementById('navigation');
+const currentPage = location.pathname;
+
+menu.querySelectorAll('a').forEach(link => {
+  if (link.getAttribute('href') === currentPage) {
+    link.classList.add('navigation__link--current');
+  }
+});
