dcda89c26816f1c71b34b9aee9b951de6b819863
ArtemTerzi <111956601+ArtemTerzi@users.noreply.github.com>
2023-03-05 13:19:04 +0000

    Deploying to gh-pages from @ ArtemTerzi/project-15@6ace244bb0381fe04e333aa83ab8d8f632ca9397 🚀

--- read.86ad9bac.js
+++
@@ -1,2 +0,0 @@
-var search=document.querySelector(".header__form"),header=document.querySelector(".search__btn-ds");search.addEventListener("click",(function(e){e.stopPropagation(),this.classList.add("search--active")})),header.addEventListener("click",(function(){search.classList.remove("search--active")}));
-//# sourceMappingURL=read.86ad9bac.js.map
