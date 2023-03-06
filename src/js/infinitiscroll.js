31c0669f17d81ef58049dab621ad761f98126952
Maria Popova <0979867251m@gmail.com>
2023-03-06 17:40:59 +0200

    scroll-infiniti

--- src/js/infinitiscroll.js
+++ src/js/infinitiscroll.js
@@ -1,17 +1,32 @@
-// window.addEventListener('scroll', debounce(e => {
-//       const q = refs.input.value;
-//       const documentRect = document.documentElement.getBoundingClientRect();
-//       if (documentRect.bottom < document.documentElement.clientHeight + 1000) {
-//         page += 1;
-//         fetchData(q, page)
-//           .then(markup => refs.gallery.insertAdjacentHTML('beforeend', markup))
-//           .then(data => {
-//             gallery.refresh();
-//           })
-//           .catch(error => {
-//             console.error(error);
-//             refs.loadMoreBtn.classList.add('hidden');
-//           });
-//       }
-//     }, 300)
-//   );
+spinner: document.querySelector('.spinner');
+
+window.addEventListener(
+    'scroll',
+    debounce(e => {
+      const q = refs.input.value;
+      const documentRect = document.documentElement.getBoundingClientRect();
+      const clientWidth = document.documentElement.clientWidth;// ширина окна за вычетом полосы прокрутки
+  if (clientWidth <= 780){
+    // alert(clientWidth);
+    if (documentRect.bottom < document.documentElement.clientHeight + 200) {
+        page += 1;
+        fetchData(q, page) // -------------
+          .then(markup => refs.gallery.insertAdjacentHTML('beforeend', markup))
+          .then(data => {
+            refs.spinner.classList.remove('hidden');
+            gallery.refresh();
+          })
+          .catch(error => {
+            console.error(error);
+            refs.spinner.classList.add('hidden');
+          });
+      }
+  }
+   else 
+   alert(
+    'PAGINATION'
+  );
+  refs.spinner.classList.add('hidden');
+    }, 300)
+  );
+  
