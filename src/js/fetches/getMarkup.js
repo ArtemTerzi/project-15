686a7867ff3e6336ce1b919c683b3db813303170
Bogdan Ryzhko <m3meizumini@gmail.com>
2023-03-06 18:39:13 +0200

    add styles

--- src/js/fetches/getMarkup.js
+++ src/js/fetches/getMarkup.js
@@ -1,24 +1,24 @@
 export function getMarkup(arr) {
   const defaultImg = `https://cdn.create.vista.com/api/media/small/251043028/stock-photo-selective-focus-black-news-lettering`;
   return arr.map(({ img, title, text, date, link, section }) => {
+
     return `
-        <div class="home__list-item">
-            <div class="home__list-img">
-              <p class="home__list-section">${section}</p>
-              <img src="${defaultImg && img}" alt="${title}">
-            </div>
-            <div class="news-item__buttons">
-              <button type="button" class="news-item__btn">Add to favorite</button>
-              <button type="button" class="news-item__category">Job searching</button>
-            </div>
-            <div class="news-item__wrapper-descr">
-              <p class="news-item__title">${title}</p>
-              <p class="news-item__text">${text}</p>
-            </div>
-            <div class="news-item__wrapper-date">
-              <p class="news-item__date">${date}</p>
-              <a href="${link}"class="news-item__link">Read more</a>
-            </div>
-        </div>`
+    <li class="home__list-item">
+      <div class="home__list-top">
+          <p class="home__list-section">${section}</p>
+          <img width="353" height="395" class="home__list" src=${img} alt=${title}>
+          <button type="button" id="like" class="add-btn">Add to favorite</button>
+          <button type="button" id="dislike" class="remove-button invisible-button">Remove from favorite</button>
+      </div>
+      <div class="home__list-description">
+        <h2 class="home__list-title">${title}</h2>
+        <p class="home__list-text">${text}</p>
+      </div>
+      <div class="home__list-footer">
+        <p class="home__list-date">${date}</p>
+        <a href=${link} class="home__list-link">Read more</a>
+      </div>
+    </li>
+    `
   }).join("");
 }
