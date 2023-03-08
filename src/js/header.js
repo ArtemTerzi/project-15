00fca54d908978a93b1600c86ca027e83c52b955
Artem Terzi <terzi.artem.yuriyovych@gmail.com>
2023-03-08 22:49:51 +0200

    -stopPropagation

--- src/js/header.js
+++ src/js/header.js
@@ -1,16 +1,14 @@
 const search = document.querySelector('.header__form');
 const header = document.querySelector('.header');
 
-search.addEventListener('click', function(e) {
-    e.stopPropagation();
-    this.classList.add('search--active')
+search.addEventListener('click', function (e) {
+  // e.stopPropagation();
+  this.classList.add('search--active');
 });
 
-header.addEventListener('click', function() {
-    search.classList.remove('search--active')
+header.addEventListener('click', function () {
+  search.classList.remove('search--active');
 });
-
-
 
 const menu = document.getElementById('navigation');
 const currentPage = location.pathname;
