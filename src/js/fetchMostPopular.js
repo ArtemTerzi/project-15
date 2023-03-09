9caacad6e27f808328a369bbef3136ab5279b595
Artem Terzi <terzi.artem.yuriyovych@gmail.com>
2023-03-09 12:41:43 +0200

    change fetch by 1 day for good pictures

--- src/js/fetchMostPopular.js
+++ src/js/fetchMostPopular.js
@@ -3,7 +3,7 @@ import { options } from './refs.js';
 import axios from 'axios';
 
 const { API_KEY } = options;
-const mostPopularUrl = `https://api.nytimes.com/svc/mostpopular/v2/viewed/30.json?api-key=${API_KEY}`;
+const mostPopularUrl = `https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=${API_KEY}`;
 export function fetchMostPopular() {
   return axios.get(mostPopularUrl);
 }
