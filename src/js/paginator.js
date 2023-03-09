ca616ad0da7729a7d8ceea8e458cd34060b894eb
AllaGrey <alla.basanko2012@gmail.com>
2023-03-09 20:04:06 +0200

    розкоментувала виклик погоди в пагінаторі

--- src/js/paginator.js
+++ src/js/paginator.js
@@ -93,43 +93,52 @@ export class Paginator {
   }
 
   async makeFetchForSearchByQuery() {
-    await axios.get(this.URL).then(answer => {
-      const {
-        data: {
-          response: { docs },
-        },
-      } = answer;
-      const responseURL = answer.config.url;
-      this.data = getNormalizeResponse(docs, responseURL);
-      this.container.innerHTML = getMarkup(this.data);
-    });
-    // .then(startWeather);
+    await axios
+      .get(this.URL)
+      .then(answer => {
+        const {
+          data: {
+            response: { docs },
+          },
+        } = answer;
+        const responseURL = answer.config.url;
+        this.data = getNormalizeResponse(docs, responseURL);
+        this.container.innerHTML = getMarkup(this.data);
+      })
+      .then(startWeather);
   }
 
   async makeFetchForSearhByCategory() {
-    await axios.get(this.URL).then(response => {
-      const {
-        data: { results },
-      } = response;
-      this.URL = response.config.url;
-      this.data = getNormalizeResponse(results, this.URL);
-      this.container.innerHTML = getMarkup(this.data);
-    });
-    // .then(startWeather);
+    await axios
+      .get(this.URL)
+      .then(response => {
+        const {
+          data: { results },
+        } = response;
+        this.URL = response.config.url;
+        this.data = getNormalizeResponse(results, this.URL);
+        this.container.innerHTML = getMarkup(this.data);
+      })
+      .then(startWeather);
   }
 
   async makeFetchForSearhByPopular() {
-    await axios.get(this.URL).then(response => {
-      const {
-        data: { results },
-      } = response;
-      this.URL = response.config.url;
-      this.data = getNormalizeResponse(results, this.URL);
-      this.container.innerHTML = getMarkup(
-        this.data.splice(this.itemsPerPage * (this.page - 1), this.itemsPerPage)
-      );
-    });
-    // .then(startWeather);
+    await axios
+      .get(this.URL)
+      .then(response => {
+        const {
+          data: { results },
+        } = response;
+        this.URL = response.config.url;
+        this.data = getNormalizeResponse(results, this.URL);
+        this.container.innerHTML = getMarkup(
+          this.data.splice(
+            this.itemsPerPage * (this.page - 1),
+            this.itemsPerPage
+          )
+        );
+      })
+      .then(startWeather);
   }
 
   show() {
