2c9dd900f3f25fc8902e179a7a7fe4f679c52746
Artem Terzi <terzi.artem.yuriyovych@gmail.com>
2023-03-09 12:09:31 +0200

    error page fixed

--- src/js/renderMarkupError.js
+++ src/js/renderMarkupError.js
@@ -1,6 +1,7 @@
 import { getMarkupError } from './error';
 
 export function renderMarkupError(parent) {
-  const parEl = document.querySelector(`${parent}`);
+  // const parEl = document.querySelector(`${parent}`);
+  const parEl = document.querySelector('.home__list');
   parEl.innerHTML = `${getMarkupError()}`;
 }
