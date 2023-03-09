3d707f259873343d92dd5f009f0feade1a96bb3f
Artem Terzi <terzi.artem.yuriyovych@gmail.com>
2023-03-10 01:02:29 +0200

    inputFilterFunc changed

--- src/js/inputFilterFunc.js
+++ src/js/inputFilterFunc.js
@@ -1,6 +1,25 @@
-// import { makeArrNewsForPageRead } from './read';
-// const searchInput = document.querySelector('.search__input');
-// if (window.location.pathname !== '/index.html') {
-//   searchInput.addEventListener('input', makeArrNewsForPageRead);
-//   console.log('hi');
-// }
+import { makeArrNewsForPageRead } from './read';
+import { refs } from './refs';
+import { getMarkup } from './fetches/getMarkup';
+
+const readPageList = document.querySelector('.readPage-list');
+// const list = document.querySelector('read-page-list__list');
+
+function onSearchFilter() {
+  const searchInput = document.querySelector('.search__input');
+  if (window.location.pathname !== '/index.html') {
+    searchInput.addEventListener('input', filtered);
+  }
+}
+
+function filtered(event) {
+  const value = event.currentTarget.value;
+  const localArray = JSON.parse(
+    localStorage.getItem('news-state-local-storage')
+  );
+
+  const filteredArray = localArray.filter(({ title }) => title.includes(value));
+  readPageList.innerHTML = getMarkup(filteredArray);
+}
+
+export { onSearchFilter };
