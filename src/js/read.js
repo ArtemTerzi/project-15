dd1b3956dbf4a3581e4ec6acf23f4a92fe8fbaea
Bogdan Ryzhko <m3meizumini@gmail.com>
2023-03-10 13:57:21 +0200

    remove weather from read page and home.html, remove padding-bottom from weather item

--- src/js/read.js
+++ src/js/read.js
@@ -263,5 +263,12 @@ function openListsReadNews(e) {
 
 export { onBtnReadMore, makeArrNewsForPageRead };
 
-let list = document.querySelector('.read-page-list__list');
-list.children[0].remove();
+let list = document.querySelector('.read-page-list__list').children;
+
+const removeClass = "weather-container";
+
+for (let item of list) {
+  if (item.classList.contains(removeClass)) {
+    item.remove()
+  }
+}
