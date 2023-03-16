1fb4c63f68ffa6d813372dc78bfe14908190a05d
Artem Terzi <terzi.artem.yuriyovych@gmail.com>
2023-03-16 10:40:23 +0200

    firebaseData switched off

--- src/js/firebaseData.js
+++ src/js/firebaseData.js
@@ -1,26 +1,26 @@
-//themeswiper
-import { initializeApp } from 'firebase/app';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import {
-  doc,
-  setDoc,
-  getFirestore,
-  collection,
-  addDoc,
-  getDocs,
-} from 'firebase/firestore';
+// //themeswiper
+// import { initializeApp } from 'firebase/app';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import {
+//   doc,
+//   setDoc,
+//   getFirestore,
+//   collection,
+//   addDoc,
+//   getDocs,
+// } from 'firebase/firestore';
 
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signOut,
-  signInWithEmailAndPassword,
-  signInWithRedirect,
-  GoogleAuthProvider,
-  getRedirectResult,
-} from 'firebase/auth';
+// import {
+//   getAuth,
+//   createUserWithEmailAndPassword,
+//   signOut,
+//   signInWithEmailAndPassword,
+//   signInWithRedirect,
+//   GoogleAuthProvider,
+//   getRedirectResult,
+// } from 'firebase/auth';
 
-import { app, firebaseConfig, auth } from './firebaseSignUp.js';
+// import { app, firebaseConfig, auth } from './firebaseSignUp.js';
 
 // const firebaseConfig = {
 //   apiKey: 'AIzaSyCkFvds2hJc9mUmc-_oFpHYsUcBiyM7opU',
@@ -31,9 +31,9 @@ import { app, firebaseConfig, auth } from './firebaseSignUp.js';
 //   appId: '1:220245139302:web:1c8b0e871bf6f9c36032a7',
 // };
 
-const provider = new GoogleAuthProvider();
-const app = initializeApp(firebaseConfig);
-const auth = getAuth();
-const db = getFirestore(app);
+// const provider = new GoogleAuthProvider();
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth();
+// const db = getFirestore(app);
 
-export { firebaseConfig, app, auth, db, provider };
+// export { firebaseConfig, app, auth, db, provider };
