6b378eed7b56b9904a6adf7e89c678ce668a0086
Artem Terzi <terzi.artem.yuriyovych@gmail.com>
2023-03-16 10:38:30 +0200

    firebaseConfig commented

--- src/js/firebaseData.js
+++ src/js/firebaseData.js
@@ -22,14 +22,14 @@ import {
 
 import { app, firebaseConfig, auth } from './firebaseSignUp.js';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyCkFvds2hJc9mUmc-_oFpHYsUcBiyM7opU',
-  authDomain: 'my-awesome-project-news.firebaseapp.com',
-  projectId: 'my-awesome-project-news',
-  storageBucket: 'my-awesome-project-news.appspot.com',
-  messagingSenderId: '220245139302',
-  appId: '1:220245139302:web:1c8b0e871bf6f9c36032a7',
-};
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyCkFvds2hJc9mUmc-_oFpHYsUcBiyM7opU',
+//   authDomain: 'my-awesome-project-news.firebaseapp.com',
+//   projectId: 'my-awesome-project-news',
+//   storageBucket: 'my-awesome-project-news.appspot.com',
+//   messagingSenderId: '220245139302',
+//   appId: '1:220245139302:web:1c8b0e871bf6f9c36032a7',
+// };
 
 const provider = new GoogleAuthProvider();
 const app = initializeApp(firebaseConfig);
